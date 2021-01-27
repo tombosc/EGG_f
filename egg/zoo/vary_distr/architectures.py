24d84d56c76a8277a80fc63c730e77dccae34e71
Tom Bosc <bosc.tom@gmail.com>
2021-01-27 15:29:53 +0100

    Fix: use mask to prevent choice of invalid objects
    Since number of distractors vary, we need to prevent comparison against
    padded vectors with the mask.

--- egg/zoo/vary_distr/architectures.py
+++ egg/zoo/vary_distr/architectures.py
@@ -18,9 +18,11 @@ class PragmaticSenderSimple(nn.Module):
     def __init__(self, n_hidden, n_features, dim_embed, max_value):
         super().__init__()
         # as a first approx, embed all different features using same embedding
+        self.padding_value = 0
+        # since 0 is a padding value, all the actual features go from 1 to
+        # max_value+1 (not included)
         self.embeddings = nn.Embedding(max_value+1, dim_embed)
-                #padding_idx=torch_nan)
-        self.fc1 = nn.Linear(n_features*dim_embed, n_hidden)
+        self.fc1 = nn.Linear(n_features * dim_embed, n_hidden)
 
     def forward(self, x):
         # TODO clean
@@ -28,7 +30,7 @@ class PragmaticSenderSimple(nn.Module):
         #  x, n = x_and_n
         bs, max_L, n_features = x.size()
         # create mask set to 1 where iff x[i, j] > n[i] elsewhere 0
-        mask = (x == 0).bool()
+        mask = (x == self.padding_value).bool()
         n_distractors = (~mask[:, :, 0]).long().sum(1) # (bs,)
         # visual debug:
         #  print(n)
@@ -53,17 +55,20 @@ class DiscriReceiverEmbed(nn.Module):
     """
     def __init__(self, n_features, n_hidden, dim_embed, n_embeddings):
         super().__init__()
+        self.padding_value = 0
         self.embeddings = nn.Embedding(
-            n_embeddings + 1, dim_embed, padding_idx=0,
+            n_embeddings + 1, dim_embed, padding_idx=self.padding_value,
         )
         self.fc1 = nn.Linear(n_features*dim_embed, n_hidden)
 
     def forward(self, x, _input):
-        mask = torch.all(_input == 0, dim=-1)
+        # mask: (bs, L) set to False iff all features are padded
+        mask = torch.all(_input == self.padding_value, dim=-1)
         _input = self.embeddings(_input)
         bs, n_dist, n_feat, embed_dim = _input.size()
         _input = _input.view((bs, n_dist, -1))
-        embedded_input = self.fc1(_input).tanh()
-        dots = torch.matmul(embedded_input, torch.unsqueeze(x, dim=-1))
-        # TODO use the mask!
-        return dots.squeeze()
+        _input = self.fc1(_input).tanh()
+        #  print("mm", _input.size(), x.size())
+        dots = torch.matmul(_input, torch.unsqueeze(x, dim=-1)).squeeze()
+        dots = dots.masked_fill(mask, -float('inf'))
+        return dots
