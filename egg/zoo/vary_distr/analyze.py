d3ed9904fd4393bf7227191489ba18581bc5953c
Tom Bosc <bosc.tom@gmail.com>
2021-02-07 18:42:55 -0500

    Change analyze script output to json

--- egg/zoo/vary_distr/analyze.py
+++ egg/zoo/vary_distr/analyze.py
@@ -1,8 +1,9 @@
 import torch
 import argparse
 import os
+import json
 from .data_readers import Data
-import matplotlib.pyplot as plt
+import numpy as np
 
 parser = argparse.ArgumentParser()
 parser.add_argument("interaction_file", type=str)
@@ -10,11 +11,16 @@ args = parser.parse_args()
 exps_root = os.environ["EGG_EXPS_ROOT"]
 data = torch.load(exps_root + '/' + args.interaction_file)
 n_nec = Data.n_necessary_features(data.sender_input)
-#  for n, inp, rinp, l, len_msg in zip(n_nec, data.sender_input, data.receiver_input,
-#          data.labels, data.message_length):
-#      print('---------')
-#      print("necessary={}, label={}".format(n, l))
-#      print(inp)
-#      print(rinp)
-plt.scatter(n_nec, data.message_length, alpha=0.05)
-plt.show()
+
+n_nec = n_nec.float().tolist()
+lens = data.message_length.float().tolist()
+n_nec_unique = sorted(list(set(n_nec)))
+for i, n in enumerate(n_nec_unique):
+    data = np.asarray([l for l, n_ in zip(lens, n_nec) if n_ == n])
+    results = {
+        'n_necessary': n,
+        'n_datapoints': len(data),
+        'mean_data': np.mean(data),
+        'std_data': np.std(data),
+    }
+    print(json.dumps(results))
