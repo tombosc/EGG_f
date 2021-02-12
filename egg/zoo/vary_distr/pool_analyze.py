71c8ae1328e6a61b9a8994d4e1ec982b81b7b353
Tom Bosc <bosct@mila.quebec>
2021-02-11 19:46:02 -0500

    Improve very slightly analyze

--- egg/zoo/vary_distr/pool_analyze.py
+++ egg/zoo/vary_distr/pool_analyze.py
@@ -16,10 +16,11 @@ def print_lengths(fn):
     with open(fn, 'r') as f:
         lines = [json.loads(line) for line in f]
         for i, line in enumerate(lines):
-            print("{:.2f}±{:.2f} ({}); δ={:.2f}".format(
-                line['mean_data'], line['std_data'], line['n_datapoints'],
-                line['mean_data'] - lines[0]['mean_data'],
-            ))
+            if type(line) == dict:
+                print("{:.2f}±{:.2f} ({}); δ={:.2f}".format(
+                    line['mean_data'], line['std_data'], line['n_datapoints'],
+                    line['mean_data'] - lines[0]['mean_data'],
+                ))
             first = False
     print()
 
@@ -28,6 +29,7 @@ def print_lengths(fn):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("exp_dir", type=str)
+    parser.add_argument("file_name", type=str)
     args = parser.parse_args()
 
     exps_root = os.environ["EGG_EXPS_ROOT"]
@@ -36,5 +38,5 @@ if __name__ == "__main__":
     print(args.exp_dir)
     if acc > 0.3:
         print("Acc={}".format(acc))
-        results = os.path.join(exps_root, args.exp_dir, 'analyze_res_ep1000')
+        results = os.path.join(exps_root, args.exp_dir, args.file_name)
         print_lengths(results)
--- pool_analyze.sh
+++ pool_analyze.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-for DIR in $EGG_EXPS_ROOT/*tfm_nheads_4*; do
-    echo $DIR
-    python -m egg.zoo.vary_distr.pool_analyze $DIR
+for DIR in $EGG_EXPS_ROOT/sd*; do
+    python -m egg.zoo.vary_distr.pool_analyze $DIR analyze_res_ep1500
 done
