7693cc82329c0a7441ac6a59360fc269595197c1
Tom Bosc <bosct@mila.quebec>
2021-05-23 18:39:23 -0400

    Minor config changes for **past** exps

--- gpu_packing.sh
+++ gpu_packing.sh
@@ -5,5 +5,5 @@
 #SBATCH --gres=gpu:rtx8000:1
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=18G
-#SBATCh --time=3:00:00
+#SBATCh --time=8:00:00
 srun -l --output=slurm-%j-%t.out --multi-prog gpu_packing.conf
